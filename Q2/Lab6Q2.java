7bbbae3bc8d31a539dcc36080593856b4e2e3b3b
IT26102348 <it26102348@my.sliit.lk>
2026-09-23 16:01:26 +0530

    Add files via upload

---
+++ Q2/Lab6Q2.java
@@ -0,0 +1,12 @@
+public class IT26102348Lab6Q2A {
+    public static void main(String[] args) {
+        int count = 1;
+
+        
+        while (count <= 10) {
+            System.out.print(count + " ");
+            count++;
+        }
+        System.out.println(); 
+    }
+}
